ea9432b1e263e290c122c7caec5c577fceeb7394
levintovich-devops <levintovichvlad@gmail.com>
2026-08-13 14:01:06 +0300

    Clean up platformPipeline.groovy by removing RegistryConfig
    Removed unused RegistryConfig initialization from the pipeline.

--- vars/platformPipeline.groovy
+++ vars/platformPipeline.groovy
@@ -1,13 +1,3 @@
-import com.platform.jenkins.config.RegistryConfig
-
 def call(Map config = [:]) {
     echo "Step 1"
-
-    RegistryConfig registryConfig = new RegistryConfig(
-        type: 'dockerhub',
-        url: '',
-        namespace: ''
-    )
-
-    echo "Step 2"
 }
