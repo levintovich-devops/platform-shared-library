37321bab777a4f14f8b7fed2de41b2a87862e954
levintovich-devops <levintovichvlad@gmail.com>
2026-08-13 13:55:12 +0300

    Refactor platformPipeline to include RegistryConfig

--- vars/platformPipeline.groovy
+++ vars/platformPipeline.groovy
@@ -1,3 +1,13 @@
-def call() {
-    echo "platformPipeline() works"
+import com.platform.jenkins.config.RegistryConfig
+
+def call(Map config = [:]) {
+    echo "Step 1"
+
+    RegistryConfig registryConfig = new RegistryConfig(
+        type: 'dockerhub',
+        url: '',
+        namespace: ''
+    )
+
+    echo "Step 2"
 }
